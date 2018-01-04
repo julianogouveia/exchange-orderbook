20ea1e33e1bd1c2fcf4f0786586f9c0054197ecb
Juliano Gouveia <jgouveiadev@gmail.com>
2018-01-04 11:00:44 -0800

    Display the correct fields on Currency Gateway admin

--- exchange_payments/models.py
+++ exchange_payments/models.py
@@ -16,6 +16,9 @@ class CurrencyGateway(TimeStampedModel, BaseModel):
 	currency = models.ForeignKey('exchange_core.Currencies', related_name='gateway', on_delete=models.CASCADE)
 	gateway = models.CharField(max_length=50, choices=settings.SUPPORTED_PAYMENT_GATEWAYS)
 
+	def __str__(self):
+		return self.currency.name
+
 	class Meta:
 		verbose_name = _("Currency Gateway")
 		verbose_name_plural = _("Currencies Gateway")
@@ -23,8 +26,13 @@ class CurrencyGateway(TimeStampedModel, BaseModel):
 
 @admin.register(CurrencyGateway)
 class CurrencyGatewayAdmin(admin.ModelAdmin):
+	list_display = ('currency', 'symbol', 'gateway')
+
+	def symbol(self, o):
+		return o.currency.symbol
+
 	def has_add_permission(self, request):
 		return False
 
-	def has_delete_permission(self, request):
+	def has_delete_permission(self, *args, **kwargs):
 		return False
