0846f54f280f27f399d38c7ab741af7a48694d19
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-06-16 18:30:40 -0300

    adjusted price in markets

--- exchange_orderbook/views.py
+++ exchange_orderbook/views.py
@@ -123,7 +123,7 @@ class MarketsView(View):
                 'currency': market.currency.symbol,
                 'min_price': market.min_price,
                 'max_price': market.max_price,
-                'price': price,
+                'price': '{:8f}'.format(price),
                 'volume': volume
             })
 
