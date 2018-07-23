a2174920fb8b40ff1e368ec5ab1e930a3b5c0dd7
Juliano Gouveia <juliano@neosacode.com>
2018-07-23 13:58:09 -0300

    Add stock chart height

--- exchange_orderbook/context_processors.py
+++ exchange_orderbook/context_processors.py
@@ -4,4 +4,5 @@ from django.conf import settings
 def exchange(request):
     return {
         'STOCK_CHART_THEME': settings.STOCK_CHART_THEME
+        'STOCK_CHART_HEIGHT': settings.STOCK_CHART_HEIGHT
     }
